b19529d7546d52718e83f64842b1125cfea48ad1
Abdullah <abdullahrehman1019@gmail.com>
2026-06-15 14:55:27 +0500

    Create, Get Task done

---
+++ lib/views/task/create_task.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:saud_backend/models/task.dart';
+import 'package:saud_backend/services/task.dart';
+
+class CreateTask extends StatefulWidget {
+  const CreateTask({super.key});
+
+  @override
+  State<CreateTask> createState() => _CreateTaskState();
+}
+
+class _CreateTaskState extends State<CreateTask> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Task"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(
+              hint: Text("Title")
+            ),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(
+              hint: Text("Description")
+            ),
+          ),
+          isLoading ? Center(child: CircularProgressIndicator(),)
+         : ElevatedButton(onPressed: ()async{
+            try{
+              isLoading = true;
+              setState(() {});
+              await TaskServices().createTask(
+                TaskModel(
+                  title: titleController.text.toString(),
+                  description: descriptionController.text.toString(),
+                  isCompleted: false,
+                  createdAt: DateTime.now().millisecondsSinceEpoch
+                )
+              ).then((value){
+                isLoading = false;
+                setState(() {});
+                showDialog(context: context, builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Text("Task Created Successfully"),
+                    actions: [
+                      TextButton(onPressed: (){
+                        Navigator.pop(context);
+                        Navigator.pop(context);
+                      }, child: Text("Ok"))
+                    ],
+                  );
+                },);
+              });
+            }catch(e){
+              isLoading = false;
+              setState(() {});
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(e.toString())));
+            }
+          }, child: Text("Create Task"))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/task/get_all_task.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:saud_backend/models/task.dart';
+import 'package:saud_backend/services/task.dart';
+import 'package:saud_backend/views/task/create_task.dart';
+
+class GetAllTask extends StatelessWidget {
+  const GetAllTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Get All Task"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> CreateTask()));
+      },child: Icon(Icons.add),),
+      body: StreamProvider.value(
+          value: TaskServices().getAllTask(),
+          initialData: [TaskModel()],
+          builder: (context, child){
+            List<TaskModel> taskList = context.watch<List<TaskModel>>();
+            return ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Icon(Icons.task),
+                title: Text(taskList[index].title.toString()),
+                subtitle: Text(taskList[index].description.toString()),
+              );
+            },);
+          },
+      ),
+    );
+  }
+}
